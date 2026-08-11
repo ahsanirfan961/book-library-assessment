1004f27b9f876964f39166c5dad1e35be98cdcc0
ahsanirfan961 <ahsanirfan961@gmail.com>
2026-08-12 00:00:09 +0500

    Add BookGrid component

---
+++ frontend/src/components/BookGrid.jsx
@@ -0,0 +1,15 @@
+import BookCard from "./BookCard";
+
+export default function BookGrid({ books }) {
+    if (!books.length) {
+        return <p className="text-gray-600">No books found.</p>;
+    }
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {books.map(book => (
+                <BookCard key={book.id} book={book} />
+            ))}
+        </div>
+    );
+}
