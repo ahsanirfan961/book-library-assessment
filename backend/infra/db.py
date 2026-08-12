ccd8bf9a8a652f0723a1112e9fc261a505c7cbdc
ahsanirfan961 <ahsanirfan961@gmail.com>
2026-08-12 14:28:47 +0500

    Backend bootstrap with SQL alchmey

---
+++ backend/infra/db.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL)
+AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_db():
+    async with AsyncSession() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
