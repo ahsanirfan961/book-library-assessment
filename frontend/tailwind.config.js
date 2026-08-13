8e756389848d54db0ffa6b196a5134d678d34360
ahsanirfan961 <ahsanirfan961@gmail.com>
2026-08-13 20:25:55 +0500

    Fix confiuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+            gocake: ['GoCake', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
